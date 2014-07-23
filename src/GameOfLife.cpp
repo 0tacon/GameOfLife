9707d10ed62664a629d7b59bf3ba6000d54b37f5
0tacon <daniel.forde001@gmail.com>
2014-07-23 19:26:47 +0200

    Added GNU GPL licence

--- src/GameOfLife.cpp
+++ src/GameOfLife.cpp
@@ -1,3 +1,22 @@
+/*
+
+Copyright 2014 Daniel Forde
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
@@ -13,25 +32,6 @@
 
 int main ()
 {
-    //std::vector<std::pair<uint8_t, uint8_t> > initial_state;
-
-    /* GLIDER */
-    /*initial_state.push_back(std::make_pair(0, 2));
-    initial_state.push_back(std::make_pair(1, 2));
-    initial_state.push_back(std::make_pair(2, 2));
-    initial_state.push_back(std::make_pair(2, 1));
-    initial_state.push_back(std::make_pair(1, 0));*/
-
-    /* PULSAR */
-    /*std::vector<uint8_t> x = {1,6,8,13}, y = {3,4,5,9,10,11};
-    for (uint8_t i = 0; i < x.size(); i++)
-        for (uint8_t j = 0; j < y.size(); j++)
-        {
-            initial_state.push_back(std::make_pair(y[j],x[i]));
-            initial_state.push_back(std::make_pair(x[i],y[j]));
-
-        }*/
-
     GameOfLife game(165, 40);
 
     game.Play();
